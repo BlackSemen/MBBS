a806fb93c94c9dc1b28f74c81aad0d3040cf0cf9
BlackSemen <416090559@qq.com>
2018-01-28 14:41:15 +0800

    basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+          Create By Mengyixi <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+    </div>
+</footer>
